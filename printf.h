0d07a75d96b6566c0f3f291bfbf7b04cc37c74a0
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-10-15 21:51:16 +0100

    The prototype dot header file

---
+++ printf.h
@@ -0,0 +1,125 @@
+#ifndef _PRINT_H
+#define _PRINT_H
+
+/* Standard C library */
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/* Macros Definition */
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define FIELD_BUF_SIZE 50
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define COVERRT_UNSIGNED	2
+
+/**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsined value
+ *
+ * @plus_flag: on if plus_flag specified
+ *
+ * @space_flag: on if hashtag_flag specified
+ *
+ * @hashtag_flag: on if _flag specified
+ *
+ * @zero_flag: on if _flag specied
+ *
+ * @minus_flag: on if _flag specified
+ *
+ * @width_flag: field width specified
+ *
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ *
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+	unsigned int unsign		:1;
+	unsigned int plus_flag		:1;
+	unsigned int space_flag		:1;
+	unsigned int hashtag_flag	:1;
+	unsigned int zero_flag		:1;
+	unsigned int minus_flag		:1;
+	unsigned int width_flag;
+	unsigned int precision;
+	unsigned int h_modifier		:1;
+	unsigned int l_modifier		:1;
+} paramas_t;
+
+/**
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ *
+ * @f: The associated function
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, paramas_t *);
+} specifier_t;
+
+/* put.c prototypes */
+int _puts(char *str);
+int _putchar(int c);
+
+/* print_functions.c prototypes */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percentage(va_list ap, params_t *params);
+int print_S(va_list ap, params_t *params);
+
+/* number.c prototypes */
+char *convert(long int num, int base, int flags, params_t, *params);
+int print_unsigned(va_list ap, params_t *params);
+itn print_address(va_list ap, params_t *params);
+
+/* specifier.c prototypes */
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, params_t *params, va_list ap);
+
+/* convert_number.c prototypes */
+int print_hex(va_list ap, params_t *params);
+int print_HEX(char *s, va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, paramas_t *params);
+
+/* simple_printers.c prototypes */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+/* print_number.c prototype */
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t params);
+int print_number_left_shift(char *str, params_t *params);
+
+/* params.c prototype */
+void init_params(params_t *params_t, va_list ap);
+
+/* string_fields.c prototypes */
+char *get_precision(char *p, params_t *params, va_list ap);
+
+/* _printf.c prototype */
+int _printf(connst char *format, ...);
+
+#endif
